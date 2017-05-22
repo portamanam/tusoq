b1a0eba62588d5067fa6f0f19d52ff998abe484f
portaman <portaman.dev@gmail.com>
2017-05-22 14:56:02 +0300

    config added

---
+++ config.js
@@ -0,0 +1,13 @@
+/*  
+ *  Name: config.js
+ *  Function: return config of our APP
+ */
+
+// Export
+module.exports = {
+  'db' : {
+    'ip' : '127.0.0.1',
+    'port' : '27017',
+    'name' : 'friendlistApp'
+  }
+};
